7250fbc7f2b3438bae1da3204d3c85038defcc19
VedantDewangan <135214899+VedantDewangan@users.noreply.github.com>
2024-05-03 14:21:14 +0530

    Update ConnectionDB.js

--- server/ConnectionDB.js
+++ server/ConnectionDB.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const ConnectionDB = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect("mongodb+srv://vedantdewangan75:0BEIuNAENR2SpSLj@cluster0.n4mjox1.mongodb.net/vedantdewangan75?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Connection with DATABASE Successfull");
     } catch (error) {
         console.log("Connection with DATABASE Failed");
